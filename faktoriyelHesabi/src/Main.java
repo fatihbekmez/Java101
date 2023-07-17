d36e62feb4dc471875ad6f93da940a56fd0ddf48
Fatih Bekmez <132763414+fatihbekmez@users.noreply.github.com>
2023-07-17 23:07:29 +0300

    Add files via upload

--- faktoriyelHesabi/src/Main.java
+++ faktoriyelHesabi/src/Main.java
@@ -1,35 +1,60 @@
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
 
-        int i, i2, i3, i4, n, r, z, C, totaln = 1, totalr=1, totala=1, totalra=1;
-
         Scanner inp = new Scanner(System.in);
-        System.out.print("1. Sayıyı Giriniz: ");
-        n = inp.nextInt();
-
         Scanner inp2 = new Scanner(System.in);
-        System.out.print("2. Sayıyı Giriniz: ");
-        r = inp2.nextInt();
+        Scanner inp3 = new Scanner(System.in);
 
-        int a=n-r;
-        int ra = totala-totalr;
+        int normalTutar, onIkiSale,yirmiDortSale,altmisBesSale,indirimliTutar;
+        double kmUcret = 0.10;
 
-        for (i = 1; i <= n ; i++ ){
-            totaln = totaln * i;
-        }
-        for (i2= 1; i2<= r; i2++){
-            totalr = totalr * i2;
-        }
-        for (i3= 1; i3<= a; i3++) {
-            totala = totala * a;
-        }
-        for (i4=1 ; i4<= ra; i4++){
-            totalra = totalra *i4;
+        System.out.print("Mesafeyi km türünden giriniz : " );
+        int km = inp.nextInt();
+
+        System.out.print("Yaşınızı Giriniz: " );
+        int age = inp2.nextInt();
+
+        System.out.print("Yolculuk tipini giriniz ( 1 => Tek Yön, 2 => Çift Yön): ");
+        int ttype = inp3.nextInt();
+
+
+        switch (ttype){
+            case 1:
+                normalTutar = (int) (km*kmUcret);
+
+                if(age <= 12){
+                    onIkiSale = (int) normalTutar/2;
+                    System.out.print("Toplam Tutar =" + (normalTutar - onIkiSale) + " TL");
+                }else if (age >= 13 && age <=24){
+                    yirmiDortSale = (int) (normalTutar * 0.10);
+                    System.out.print("Toplam Tutar =" + (normalTutar - yirmiDortSale) + " TL");
+                }else if (age >= 65){
+                    altmisBesSale = (int) ((normalTutar/100)* 30);
+                    System.out.print("Toplam Tutar =" + (normalTutar - altmisBesSale) + " TL");
+                }
+                    break;
+
+            case 2:
+                normalTutar = (int) (km*kmUcret);
+                indirimliTutar = (int) (normalTutar * 0.20);
+
+                if(age <= 12){
+                    onIkiSale = (int) (indirimliTutar/2);
+                    System.out.print("Toplam Tutar =" + ((indirimliTutar - onIkiSale)*2) + " TL");
+                }else if (age >= 13 && age <=24){
+                    yirmiDortSale = (int) (indirimliTutar * 0.10);
+                    System.out.print("Toplam Tutar =" + ((indirimliTutar - yirmiDortSale)*2) + " TL");
+                }else if (age >= 65){
+                    altmisBesSale = (int) ((indirimliTutar/100)* 30);
+                    System.out.print("Toplam Tutar =" + ((indirimliTutar - altmisBesSale)*2) + " TL");
+                }
+                break;
+            }
+
+
         }
 
-        C = totaln / totalra;
-        System.out.println(C);
     }
-}
